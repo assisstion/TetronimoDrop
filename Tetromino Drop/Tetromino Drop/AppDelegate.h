10c3a1a559d4188d5bcec28f878bba051ac7ab76
Aditya Jha <ajha17@deerfield.edu>
2016-01-05 11:36:53 -0500

    Start

---
+++ Tetromino Drop/Tetromino Drop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tetromino Drop
+//
+//  Created by ajha17 on 1/5/16.
+//  Copyright © 2016 assisstion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tetromino Drop/Tetromino Drop/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Tetromino Drop
+//
+
+//  Copyright (c) 2016 assisstion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
